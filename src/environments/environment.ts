cad3ca1d01ac29877801eebc60f1bc6e0a3ceacc
WS <volfram@gmail.com>
2024-08-27 20:36:18 -0500

    urlapi localhost change to 127.0.0.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   multipleSelectionQuestionType: 'multiple-selection',
-  urlApiQuestionsSerpro:  window["env"]["apiUrl"] || "default",
+  urlApiQuestionsSerpro:  window["env"]["apiUrl"] || "127.0.0.1",
   msgHeader: '',
   msgGralApp: 'Bienvenido a SaberPro Admin',
 };
